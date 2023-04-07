4bd2a2d0d1a9d90cf923ec6f0ff965307fb9ca3e
your_username <patrizio.sbrozzi@gmail.com>
2023-04-07 09:04:02 +0200

    add key value to objs ok

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'formazione-fe-3';
+  arrayOfObj = [
+    {name: "Giovanni", surname: "Rossio", eta: 40},
+    {name: "Sara", surname: "Verdi", eta: 20},
+    {name: "Mario", surname: "Gialli", eta: 50}
+  ]
+  addData(obj : Array<any>, keyValueArr : Array<string>) {
+    obj.forEach(ele => {
+      ele[keyValueArr[0]] = keyValueArr[1]
+    });
+    // console.log(obj);
+  }
+  ngOnInit(): void {
+    this.addData(this.arrayOfObj, ["pippo", "pluto"]);
+    console.log(this.arrayOfObj);
+  }
 }
